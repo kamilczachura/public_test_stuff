0d9c103af1bf451e49c8673adbccda034537862b
kamilczachura <38066910+kamilczachura@users.noreply.github.com>
2025-12-08 23:44:55 +0100

    Update README.tf

--- README.tf
+++ README.tf
@@ -1,41 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
+module "shield_protection" {
+  source = "./modules/shield-protection"
 
-# variables.tf
-variable "resource_arns" {
-  description = "List of resource ARNs to protect with AWS Shield Advanced"
-  type        = list(string)
-}
-
-variable "tags" {
-  description = "Tags to assign to Shield protections"
-  type        = map(string)
-  default     = {}
-}
-
-# main.tf
-resource "aws_shield_protection" "this" {
-  for_each = toset(var.resource_arns)
-
-  name         = "shield-protection-${replace(each.value, "/.*\\//", "")}"
-  resource_arn = each.value
-
-  tags = var.tags
-}
-
-# outputs.tf
-output "protection_ids" {
-  description = "Map of resource ARNs to their Shield protection IDs"
-  value       = { for k, v in aws_shield_protection.this : k => v.id }
-}
-
-output "protected_resources" {
-  description = "List of protected resource ARNs"
-  value       = [for p in aws_shield_protection.this : p.resource_arn]
+  resource_arns = concat(
+    module.MCI-Autoscaling.alb_arns,
+    module.test.something,
+    [module.global_accelerator.accelerator_id]  # pojedyncze wartości w []
+  )
 }
