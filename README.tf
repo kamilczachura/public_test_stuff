e78fbdb1a58aa9825fe2cac7e5c0991b29ae8b17
kamilczachura <38066910+kamilczachura@users.noreply.github.com>
2025-12-09 19:06:58 +0100

    Update README.tf

--- README.tf
+++ README.tf
@@ -1,41 +1,92 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+# modules/alb-trust-store/variables.tf
+variable "name" {
+  description = "Trust store name"
+  type        = string
 }
 
-# variables.tf
-variable "global_accelerator_arns" {
-  description = "List of Global Accelerator ARNs to protect with AWS Shield Advanced"
+variable "ca_bundle_s3_bucket" {
+  description = "S3 bucket containing CA bundle"
+  type        = string
+}
+
+variable "ca_bundle_s3_key" {
+  description = "S3 key for CA bundle"
+  type        = string
+}
+
+variable "alb_arns" {
+  description = "List of ALB ARNs to associate"
   type        = list(string)
+  default     = []
 }
 
 variable "tags" {
-  description = "Tags to assign to Shield protections"
+  description = "Tags to apply"
   type        = map(string)
   default     = {}
 }
 
-# main.tf
-resource "aws_shield_protection" "ga" {
-  for_each = toset(var.global_accelerator_arns)
-
-  name         = "shield-ga-${substr(md5(each.value), 0, 8)}"
-  resource_arn = each.value
+# modules/alb-trust-store/main.tf
+resource "aws_lb_trust_store" "this" {
+  name                             = var.name
+  ca_certificates_bundle_s3_bucket = var.ca_bundle_s3_bucket
+  ca_certificates_bundle_s3_key    = var.ca_bundle_s3_key
 
   tags = var.tags
 }
 
-# outputs.tf
-output "protection_ids" {
-  description = "List of Shield protection IDs for Global Accelerators"
-  value       = [for p in aws_shield_protection.ga : p.id]
+resource "aws_lb_trust_store_revocation" "this" {
+  count = length(var.alb_arns)
+
+  trust_store_arn            = aws_lb_trust_store.this.arn
+  revocation_id              = count.index
+  s3_bucket                  = var.ca_bundle_s3_bucket
+  s3_key                     = var.ca_bundle_s3_key
+  revocation_type            = "CRL"
 }
 
-output "protected_gas" {
-  description = "List of protected Global Accelerator ARNs"
-  value       = [for p in aws_shield_protection.ga : p.resource_arn]
+resource "aws_lb_listener" "trust_store_association" {
+  count = length(var.alb_arns)
+
+  load_balancer_arn = var.alb_arns[count.index]
+  
+  mutual_authentication {
+    mode            = "verify"
+    trust_store_arn = aws_lb_trust_store.this.arn
+  }
+}
+
+# modules/alb-trust-store/outputs.tf
+output "trust_store_arn" {
+  description = "Trust store ARN"
+  value       = aws_lb_trust_store.this.arn
+}
+
+output "trust_store_id" {
+  description = "Trust store ID"
+  value       = aws_lb_trust_store.this.id
+}
+
+# Example usage in root module
+# main.tf
+module "alb_trust_store" {
+  source = "./modules/alb-trust-store"
+
+  name                  = "my-trust-store"
+  ca_bundle_s3_bucket   = "my-bucket"
+  ca_bundle_s3_key      = "ca-bundle.pem"
+  
+  alb_arns = [
+    "arn:aws:elasticloadbalancing:region:account:loadbalancer/app/alb-1/xxx",
+    "arn:aws:elasticloadbalancing:region:account:loadbalancer/app/alb-2/xxx",
+    "arn:aws:elasticloadbalancing:region:account:loadbalancer/app/alb-3/xxx",
+    "arn:aws:elasticloadbalancing:region:account:loadbalancer/app/alb-4/xxx",
+    "arn:aws:elasticloadbalancing:region:account:loadbalancer/app/alb-5/xxx",
+    "arn:aws:elasticloadbalancing:region:account:loadbalancer/app/alb-6/xxx",
+  ]
+
+  tags = {
+    Environment = "production"
+    ManagedBy   = "terraform"
+  }
 }
