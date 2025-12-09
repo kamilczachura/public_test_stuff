9c36f1b2a800d5837ddc695ac544b0ca10bcc5b5
kamilczachura <38066910+kamilczachura@users.noreply.github.com>
2025-12-09 18:54:55 +0100

    Update README.tf

--- README.tf
+++ README.tf
@@ -1,9 +1,41 @@
-module "shield_protection" {
-  source = "./modules/shield-protection"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
-  resource_arns = concat(
-    module.MCI-Autoscaling.alb_arns,
-    module.test.something,
-    [module.global_accelerator.accelerator_id]  # pojedyncze wartości w []
-  )
+# variables.tf
+variable "global_accelerator_arns" {
+  description = "List of Global Accelerator ARNs to protect with AWS Shield Advanced"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Tags to assign to Shield protections"
+  type        = map(string)
+  default     = {}
+}
+
+# main.tf
+resource "aws_shield_protection" "ga" {
+  for_each = toset(var.global_accelerator_arns)
+
+  name         = "shield-ga-${substr(md5(each.value), 0, 8)}"
+  resource_arn = each.value
+
+  tags = var.tags
+}
+
+# outputs.tf
+output "protection_ids" {
+  description = "List of Shield protection IDs for Global Accelerators"
+  value       = [for p in aws_shield_protection.ga : p.id]
+}
+
+output "protected_gas" {
+  description = "List of protected Global Accelerator ARNs"
+  value       = [for p in aws_shield_protection.ga : p.resource_arn]
 }
